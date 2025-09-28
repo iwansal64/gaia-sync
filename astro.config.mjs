b4e83681c934fc3613b99c64e5e684c057718f08
Iwansal64 <iwansal64@gmail.com>
2025-09-28 22:26:37 +0700

    [fix] Fix htttps issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,21 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [
+      tailwindcss(),
+      basicSsl({
+        name: "https certification",
+        domains: ["localhost"],
+        certDir: "../secrets/"
+      })
+    ]
   },
+
 
   integrations: [react()]
 });
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,11 +1,14 @@
-const api_url: string = "http://127.0.0.1:8080";
+const api_url: string = "https://127.0.0.1:8080";
 
-async function send_api_request({endpoint, data, method} : {endpoint: string, data?: object, method?: string}): Promise<Response> {
+async function send_api_request({endpoint, data, method} : {endpoint: string, data?: object, method?: "POST" | "GET" | "DELETE" | "PUT"}): Promise<Response> {
   //? Built the fetch data
   const response = await fetch(api_url+endpoint, {
     method: method,
     body: data ? JSON.stringify(data) : undefined,
-    credentials: "include"
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json"
+    }
   });
 
   //? Return the response
@@ -35,5 +38,16 @@ export class API {
     if(response.ok) return LoginResponseEnum.Authorized;
     else if(response.status == 401) return LoginResponseEnum.Unauthorized;
     else return LoginResponseEnum.Error;
+  }
+
+  static async logout(): Promise<boolean> {
+    //? Send post request
+    const response = await send_api_request({
+      endpoint: "/user/logout",
+      method: "POST"
+    });
+
+    //? Return the response
+    return response.ok;
   }
 }
