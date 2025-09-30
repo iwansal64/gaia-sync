b00093184392554eb8722bb4b03fc9e38f69be8c
Iwansal64 <iwansal64@gmail.com>
2025-09-30 09:10:46 +0700

    [adjust] Add vercel adapter!

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
@@ -14,6 +16,6 @@ export default defineConfig({
     ]
   },
 
-
-  integrations: [react()]
+  integrations: [react()],
+  adapter: vercel()
 });
