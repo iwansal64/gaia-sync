7339b17c0df32a8d7bcc69317be932dc02eb1543
Iwansal64 <iwansal64@gmail.com>
2025-10-08 20:05:17 +0700

    [adjust] Change from email to username and change schema

--- src/components/login_form.tsx
+++ src/components/login_form.tsx
@@ -5,7 +5,7 @@ export default function LoginForm() {
   const { setUsername, setPassword } = useLoginHooks();
   
   return <>
-    <input onChange={(e) => setUsername(e.target.value)} placeholder="email" id="email" type="text" className="bg-transparent border border-black p-4 rounded-full" />
+    <input onChange={(e) => setUsername(e.target.value)} placeholder="username" id="username" type="text" className="bg-transparent border border-black p-4 rounded-full" />
     <input onChange={(e) => setPassword(e.target.value)} placeholder="password" id="password" type="password" className="bg-transparent border border-black p-4 rounded-full" />
   </>;
 }
