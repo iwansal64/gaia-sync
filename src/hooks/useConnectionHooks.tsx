71ab5462b19d0d082fca939169622b1a695b9730
Iwansal64 <iwansal64@gmail.com>
2025-10-17 13:41:48 +0700

    [improve] Add error when user unauthorized to subs

--- src/hooks/useConnectionHooks.tsx
+++ src/hooks/useConnectionHooks.tsx
@@ -1,8 +1,10 @@
 import { create } from "zustand";
-import { Message, Client as MQTTClient } from "paho-mqtt";
+import mqtt, { type ISubscriptionGrant } from "mqtt";
 import { useEffect } from "react";
 import { useSensorDataHooks } from "./useSensorDataHooks";
 import { useUserDataHooks } from "./useUserDataHooks";
+import type { ISubackPacket } from "mqtt-packet";
+import { useToastHooks } from "./useToastHooks";
 
 export type UseConnectionHooksType = {
   isConnected: boolean,
@@ -17,29 +19,47 @@ export const useConnectionHooks = create<UseConnectionHooksType>()((set) => ({
   },
 }));
 
-function constructMessageData(message: string) {
-  const keyValuePairs: string[][] = message.split(";").map(data => data.split("="));
-  let returnValue: {
-    [key: string]: string | number
-  } = {};
 
-  keyValuePairs.forEach(([key, value]) => {
-    const possibleNumber = Number.parseFloat(value);
-    if(isNaN(possibleNumber)) {
-      returnValue[key] = value;
-    }
-    else {
-      returnValue[key] = possibleNumber;
-    }
-  });
+// Callback Function
+type toastShowMessageFunction = (data: {
+    title: string;
+    message?: string;
+    timeout?: number;
+}) => void;
 
-  return returnValue;
+function onSubscribeCallback(showMessage: toastShowMessageFunction, err: Error | null, granted?: ISubscriptionGrant[], packet?: ISubackPacket) {
+  if (packet && packet.granted[0] === 128) {
+    showMessage({
+      title: "Can't connect to the device",
+      message: "Have you configure your device yet?",
+      timeout: 10000
+    });
+    return;
+  }
+  
+  if(err) {
+    showMessage({
+      title: "Error :(",
+      message: "There's an error when connecting to the device. Please contact developer 🙏🏻",
+      timeout: 10000
+    });
+    console.error(`This is the error message dawg: ${err.message}`);
+  }
+  else {
+    showMessage({
+      title: "Connected!",
+      message: "You're connected to the system!",
+      timeout: 5000
+    });
+  }
 }
+
 
 export default function UseConnectionHooksEffect() {
   const { setIsConnected } = useConnectionHooks();
   const { accessToken, userId, deviceId } = useUserDataHooks();
   const { setData } = useSensorDataHooks();
+  const { showMessage } = useToastHooks();
 
   useEffect(() => {
     console.log("Checking Data..")
@@ -47,18 +67,38 @@ export default function UseConnectionHooksEffect() {
     console.log("Connecting..")
 
     
-    const mqttClient = new MQTTClient("mqttws.gaia-odc.digital", 443, "/", userId);
+    const mqttClient = mqtt.connect("wss://mqttws.gaia-odc.digital:443", {
+      username: userId,
+      password: accessToken,
+      reconnectPeriod: 5000,
+      resubscribe: true
+    });
 
-    mqttClient.onConnectionLost = (responseObject) => {
-      console.log("Connection lost:", responseObject.errorMessage);
+    mqttClient.on("connect", () => {
+      console.log("[MQTT] Connected Successfully!");
+
+      mqttClient.subscribe(`${deviceId}/tds`, {}, (err, granted, packet) => onSubscribeCallback(showMessage, err, granted, packet))
+      mqttClient.subscribe(`${deviceId}/ec`, {}, (err, granted, packet) => onSubscribeCallback(showMessage, err, granted, packet))
+      mqttClient.subscribe(`${deviceId}/ph`, {}, (err, granted, packet) => onSubscribeCallback(showMessage, err, granted, packet))
+      mqttClient.subscribe(`${deviceId}/tempC`, {}, (err, granted, packet) => onSubscribeCallback(showMessage, err, granted, packet))
+    })
+
+    mqttClient.on("disconnect", (data) => {
+      console.log("[MQTT] Connection lost. Reason Code: ", data.reasonCode);
       setIsConnected(false);
-    };
+    });
 
-    mqttClient.onMessageArrived = (message: Message) => {
-      const data = Number.parseFloat(message.payloadString);
+    mqttClient.on("error", (err) => {
+      console.error("[MQTT] There's an error");
+      console.error(err.message);
+    })
+
+    mqttClient.on("message", (topic, buf) => {
+      const message = buf.toString();
+      const data = Number.parseFloat(message);
       if(isNaN(data)) return;
 
-      switch (message.destinationName) {
+      switch (topic) {
         case `${deviceId}/tds`:
           setData({
             tds: data
@@ -86,30 +126,13 @@ export default function UseConnectionHooksEffect() {
         default:
           break;
       }
-    };
-
-
-    console.log("ws");
-    mqttClient.connect({
-      useSSL: true,
-      userName: userId,
-      password: accessToken,
-      onSuccess: () => {
-        console.log("MQTT Connected!");
-        mqttClient.subscribe(`${deviceId}/tds`);
-        mqttClient.subscribe(`${deviceId}/ec`);
-        mqttClient.subscribe(`${deviceId}/tempC`);
-        mqttClient.subscribe(`${deviceId}/ph`);
-      },
-      onFailure: (err) => {
-        console.error("There's an error");
-        console.error(err);
-      },
     });
 
     return (() => {
-      if(mqttClient.isConnected()) {
-        mqttClient.disconnect();
+      if(mqttClient.connected) {
+        mqttClient.end(false, {}, () => {
+          console.log("[MQTT] Disconnected Gracefully")
+        });
       }
     });
   }, []);
