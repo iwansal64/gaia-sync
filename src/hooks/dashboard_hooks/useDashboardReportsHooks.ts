25d3b080eb7bde96f5c846a74587cdca87000de6
Iwansal64 <iwansal64@gmail.com>
2025-10-22 12:21:55 +0700

    [improve] Add function to apply expiration modifier for any hooks

--- src/hooks/dashboard_hooks/useDashboardReportsHooks.ts
+++ src/hooks/dashboard_hooks/useDashboardReportsHooks.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import type { AccessedModelAIReportType } from "../../lib/model";
 import { createJSONStorage, persist } from "zustand/middleware";
-import { DATA_EXPIRATION_TIME } from "../../utils/state_manager";
+import { expiration_modifier } from "../../utils/state_manager";
 
 export type AIReportsHooksType = {
       aiReports?: AccessedModelAIReportType[];
@@ -22,8 +22,6 @@ export const useDashboardReportsHooks = create<AIReportsHooksType>()(
                         set(() => ({
                               aiReports: newAiReports,
                         }));
-
-                        sessionStorage.setItem("gaia-reports-data-timestamp", new Date().toString());
                   },
 
                   aiReportKeyword: "",
@@ -49,30 +47,7 @@ export const useDashboardReportsHooks = create<AIReportsHooksType>()(
                       }
                   },
                   merge(persistedState, currentState) {
-                        let result: Partial<AIReportsHooksType> = {};
-
-                        const reportTimestamp: string | null = sessionStorage.getItem("gaia-reports-data-timestamp");
-                        const reportTimestampDate: Date | null = reportTimestamp ? new Date(reportTimestamp) : null;
-
-                        // If the report timestamp date is not exists or the report data expired
-                        if (!reportTimestampDate || (Date.now().valueOf() - reportTimestampDate.valueOf()) > (DATA_EXPIRATION_TIME * 1000)) {
-                              // Remove session timestamp data
-                              sessionStorage.removeItem("gaia-reports-data-timestamp");
-
-                              // Reset the AI reports data
-                              result = {
-                                    ...currentState,
-                                    ...(persistedState as any),
-                                    ...result,
-                                    aiReports: currentState.aiReports,
-                              };
-                        }
-
-                        return {
-                              ...currentState,
-                              ...(persistedState as any),
-                              ...result,
-                        };
+                        return expiration_modifier<AIReportsHooksType, "aiReports", "setAIReports">(currentState, (persistedState as Partial<AIReportsHooksType>), "gaia-reports-data-created", "aiReports", "setAIReports", 60).done();
                   },
             }
       )
--- src/utils/state_manager.ts
+++ src/utils/state_manager.ts
@@ -7,3 +7,47 @@ export function resetAllStorageState() {
       useDashboardReportsHooks.persist.clearStorage();
       useUserDataHooks.persist.clearStorage();
 }
+
+type ExpirationModifierResult<T extends object> = {
+      add: <K extends keyof T, L extends keyof T>(expirationName: string, key: K, keySetter: L, expire_in_secs: number) => ExpirationModifierResult<T>
+      done: () => T
+}
+
+export function expiration_modifier<T extends object, K extends keyof T, L extends keyof T>(defaultState: T, persistedState: Partial<T>, expirationName: string, key: K, keySetter: L, expire_in: number, initial_modified_result?: Partial<T>): ExpirationModifierResult<T> {
+      let result: Partial<T> = initial_modified_result ?? {};
+      result[keySetter] = ((newData: K) => {
+            (defaultState[keySetter] as Function)(newData);
+
+            sessionStorage.setItem(expirationName, new Date().toString());
+      }) as any;
+      
+      const reportTimestamp: string | null = sessionStorage.getItem(expirationName);
+      const reportTimestampDate: Date | null = reportTimestamp ? new Date(reportTimestamp) : null;
+
+      // If the report timestamp date is not exists or the report data expired
+      if (!reportTimestampDate || (Date.now().valueOf() - reportTimestampDate.valueOf()) > (expire_in * 1000)) {
+            // Remove session timestamp data
+            sessionStorage.removeItem(expirationName);
+
+            // Reset the AI reports data
+            let modifiedKey: Partial<T> = {};
+            modifiedKey[key] = defaultState[key];
+
+            result = {
+                  ...defaultState,
+                  ...(persistedState as any),
+                  ...result,
+                  ...modifiedKey,
+            };
+      }
+
+
+      return {
+            add: <K extends keyof T, L extends keyof T>(expirationName: string, key: K, keySetter: L, expire_in: number) => expiration_modifier(defaultState, persistedState, expirationName, key, keySetter, expire_in, result),
+            done: () => ({
+                  ...defaultState,
+                  ...(persistedState as any),
+                  ...result,
+            })
+      };
+}
