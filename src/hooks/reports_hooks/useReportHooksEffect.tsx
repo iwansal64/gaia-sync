522625242a1436d55e30379035d908dbb3d52591
Iwansal64 <iwansal64@gmail.com>
2025-10-22 13:43:20 +0700

    [improve] Report data will only persist if the requested id is the same

--- src/hooks/reports_hooks/useReportHooksEffect.tsx
+++ src/hooks/reports_hooks/useReportHooksEffect.tsx
@@ -6,7 +6,7 @@ export default function UseReportHooksEffect() {
       const { currentReportId, reportData, setReportData } = useReportHooks();
       
       useEffect(() => {
-            if(!currentReportId || reportData !== null) return;
+            if(!currentReportId || (reportData !== null && currentReportId == reportData.id)) return;
             
             API.get_ai_report(currentReportId).then((data) => {
                   if(!data) return;
