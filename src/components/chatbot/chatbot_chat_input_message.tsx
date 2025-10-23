718a4567f1e7acce807bb107152fcbf9c0c1aa93
Iwansal64 <iwansal64@gmail.com>
2025-10-23 15:22:33 +0700

    [half] Chat input form UI done!

---
+++ src/components/chatbot/chatbot_chat_input_message.tsx
@@ -0,0 +1,4 @@
+export default function ChatbotChatInputMessage() {
+
+      return <input className="p-4 bg-gray-600 text-white w-full rounded-full font-semibold" placeholder="Chat here.."></input>;
+}
---
+++ src/components/chatbot/chatbot_chat_input_send.tsx
@@ -0,0 +1,8 @@
+import { IoIosSend } from "react-icons/io";
+
+export default function ChatbotChatInputSend() {
+
+      return <div className="p-4 h-fit! aspect-square flex justify-center items-center bg-gray-600 rounded-full">
+            <IoIosSend color="white" fontSize={24} />
+      </div>;
+}
